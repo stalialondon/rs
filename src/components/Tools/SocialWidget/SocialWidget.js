7a1c3ff0758225471212c49bcbd44d50414aa926
unset <unset>
2020-03-05 09:45:52 -0700

    added graph query from contentful for contentful links

--- src/components/Tools/SocialWidget/SocialWidget.js
+++ src/components/Tools/SocialWidget/SocialWidget.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 import Facebook from "../../../images/Icons/Social/Facebook"
 import Instagram from "../../../images/Icons/Social/Instagram"
 import LinkedIn from "../../../images/Icons/Social/LinkedIn"
@@ -8,31 +9,66 @@ import Twitter from "../../../images/Icons/Social/Twitter"
 import YouTube from "../../../images/Icons/Social/YouTube"
 import S from "./SocialWidget.module.scss"
 
-export default () => (
-  <div className={S.socialwidget}>
-    <a href="https://www.facebook.com/" alt="">
-      <Facebook />
-    </a>
-    <a href="https://www.instagram.com/staliajewellerylondon/" alt="Instagram">
-      <Instagram />
-    </a>
-    <a
-      href="https://www.linkedin.com/in/rebecca-savva-54933580/?originalSubdomain=uk"
-      alt="LinkedIn"
-    >
-      <LinkedIn />
-    </a>
-    <a href="https://www.pinterest.co.uk/" alt="Pinterest">
-      <Pinterest />
-    </a>
-    <a href="https://www.tumblr.com/" alt="Tumblr">
-      <Tumblr />
-    </a>
-    <a href="https://twitter.com/" alt="Twitter">
-      <Twitter />
-    </a>
-    <a href="https://www.youtube.com/" alt="YouTube">
-      <YouTube />
-    </a>
-  </div>
-)
+export default () => {
+
+  const data = useStaticQuery(graphql`
+    {
+      allContentfulGeneral {
+        edges {
+          node {
+            facebookLink
+            linkedInLink
+            tumblrLink
+            twitterLink
+            youTubeLink
+            instagramLink
+            pinterestLink  
+          }
+        }
+      }
+    }
+  `)
+
+  const {
+    facebookLink,
+    linkedInLink,
+    tumblrLink,
+    twitterLink,
+    youTubeLink,
+    instagramLink,
+    pinterestLink } = data.allContentfulGeneral.edges[0].node
+
+  return (
+    <div className={S.socialwidget}>
+      <a href={facebookLink} alt="">
+        <Facebook />
+      </a>
+      <a href={instagramLink}
+        alt="Instagram">
+        <Instagram />
+      </a>
+      <a
+        href={linkedInLink}
+        alt="LinkedIn"
+      >
+        <LinkedIn />
+      </a>
+      <a href={pinterestLink}
+        alt="Pinterest">
+        <Pinterest />
+      </a>
+      <a href={tumblrLink}
+        alt="Tumblr">
+        <Tumblr />
+      </a>
+      <a href={twitterLink}
+        alt="Twitter">
+        <Twitter />
+      </a>
+      <a href={youTubeLink}
+        alt="YouTube">
+        <YouTube />
+      </a>
+    </div>
+  )
+}
