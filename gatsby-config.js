efdf86810e17778a54de507bb14954704a9c2a8c
unset <unset>
2020-03-17 20:41:05 -0700

    added firebase to project

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,5 +45,19 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: "gatsby-plugin-firebase",
+      options: {
+        credentials: {
+          apiKey: process.env.FIREBASE_API_KEY,
+          authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+          databaseURL: process.env.FIREBASE_DATABASE_URL,
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+          messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+          appId: process.env.FIREBASE_APP_ID
+        }
+      }
+    }
   ],
 }
--- gatsby-node.js
+++ gatsby-node.js
@@ -8,10 +8,10 @@ const path = require('path');
 // Implement the Gatsby API “createPages”. This is called once the
 // data layer is bootstrapped to let plugins create pages from data.
 exports.createPages = async ({ graphql, actions, reporter }) => {
-    const { createPage } = actions
-    // Query for markdown nodes to use in creating pages.
-    const result = await graphql(
-        `
+  const { createPage } = actions
+  // Query for markdown nodes to use in creating pages.
+  const result = await graphql(
+    `
       {
         allContentfulBlogPosts {
           edges {
@@ -23,27 +23,27 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
         }
       }      
         `
-    )
+  )
 
-    // Handle errors
-    if (result.errors) {
-        reporter.panicOnBuild(`Error while running GraphQL query.`)
-        return
-    }
+  // Handle errors
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`)
+    return
+  }
 
-    // Create pages for each Contentful Post
-    const blogTemplate = path.resolve(`src/components/blog/BlogTemplate.js`)
-    result.data.allContentfulBlogPosts.edges.forEach(({ node }) => {
-        
-        const path = `blog/${node.slug}`
-        createPage({
-            path,
-            component: blogTemplate,
-            // In your blog post template's graphql query, you can use path
-            // as a GraphQL variable to query for data from the markdown file.
-            context: {
-                slug: node.slug
-            },
-        })
+  // Create pages for each Contentful Post
+  const blogTemplate = path.resolve(`src/components/blog/BlogTemplate.js`)
+  result.data.allContentfulBlogPosts.edges.forEach(({ node }) => {
+
+    const path = `blog/${node.slug}`
+    createPage({
+      path,
+      component: blogTemplate,
+      // In your blog post template's graphql query, you can use path
+      // as a GraphQL variable to query for data from the markdown file.
+      context: {
+        slug: node.slug
+      },
     })
+  })
 }
